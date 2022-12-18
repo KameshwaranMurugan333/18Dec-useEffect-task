46d64cbf23e15d98c923b11466b02a7ba69e294a
Kameshwaran Murugan <kameshwaran@crayond.co>
2022-12-18 19:13:01 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { SelectBox } from './components/SelectBox';
+
+const indiaStateOption = [{ label: "Karnataka", value: "kar" }, { label: "Tamilnadu", value: "TN" }];
+const indiaKarOption = [{ label: "Bangalore", value: "bang" }, { label: "Mangalore", value: "mang" }]
 
 function App() {
+
+  const [country, setCountry] = React.useState("");
+  const [state, setState] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  const [countryOptions] = React.useState([
+    { label: "India", value: "india" },
+    { label: "USA", value: "usa" },
+    { label: "Pakistan", value: "pakistan" }
+  ]);
+
+  const [stateOptions, setStateOptions] = React.useState([]);
+  const [cityOptions, setCityOptions] = React.useState([]);
+
+  React.useEffect(() => {
+    if (country === "india") {
+      setStateOptions(indiaStateOption);
+    }
+  }, [country]);
+
+  React.useEffect(() => {
+    if (state === "kar") {
+      setCityOptions(indiaKarOption);
+    }
+  }, [state]);
+
+  React.useEffect(()=> {
+    setMessage(`${country} -> ${state} -> ${city}`)
+  },[country, state, city])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Country */}
+      <SelectBox label='Country' value={country} onChange={setCountry} options={countryOptions} />
+
+      {/* State */}
+      <SelectBox label='State' value={state} onChange={setState} options={stateOptions} />
+
+      {/* City */}
+      <SelectBox label='City' value={city} onChange={setCity} options={cityOptions} />
+
+      <h1>{message}</h1>
     </div>
   );
 }
